5c69b58f38872d106b1ae847f73438a6e44c982e
Emine Şener <eminesener063@gmail.com>
2023-05-24 00:03:40 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,252 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Henry</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+    <!-- bootstrap 1 -->
+    <!-- Favicons -->
+    <link href="assets1/img/favicon.png" rel="icon">
+    <link href="assets1/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="assets1/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets1/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets1/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets1/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets1/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets1/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="assets1/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="assets1/css/style.css" rel="stylesheet">
+
+    <!-- =======================================================
+    * Template Name: Dewi
+    * Updated: Mar 10 2023 with Bootstrap v5.2.3
+    * Template URL: https://bootstrapmade.com/dewi-free-multi-purpose-html-template/
+    * Author: BootstrapMade.com
+    * License: https://bootstrapmade.com/license/
+    ======================================================== -->
+
+
+    <!-- bootstrap 2 -->
+    <!-- Favicons -->
+    <link href="assets2/img/favicon.png" rel="icon">
+    <link href="assets2/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.gstatic.com" rel="preconnect">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="assets2/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets2/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets2/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets2/vendor/quill/quill.snow.css" rel="stylesheet">
+    <link href="assets2/vendor/quill/quill.bubble.css" rel="stylesheet">
+    <link href="assets2/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="assets2/vendor/simple-datatables/style.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="assets2/css/style.css" rel="stylesheet">
+
+    <!-- =======================================================
+    * Template Name: NiceAdmin
+    * Updated: Mar 09 2023 with Bootstrap v5.2.3
+    * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
+    * Author: BootstrapMade.com
+    * License: https://bootstrapmade.com/license/
+    ======================================================== -->
+
+
+</head>
+
+<body>
+    <!-- ======= Header ======= -->
+    <header id="header" class="fixed-top ">
+        <div class="container d-flex align-items-center justify-content-between">
+
+        <h1 class="logo"><a href="index.php">Henry</a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+        <nav id="navbar" class="navbar">
+            <ul>
+            <li><a class="nav-link scrollto active" href="#hero">Ana Sayfa</a></li>
+            <li><a class="nav-link scrollto" href="#about">Hakkımızda</a></li>
+            <li class="dropdown"><a href="#"><span>Sayfa Rehberi</span> <i class="bi bi-chevron-down"></i></a>
+                <ul>
+                <li><a href="#rehber1">Yönetici Rehberi</a></li>
+                <li><a href="#rehber2">Çalışan Rehberi</a></li>
+                <li><a href="#rehber3">İlgilenen Rehberi</a></li>
+                </ul>
+            </li>
+            
+            <li class="dropdown"><a href="#"><span>Giriş</span> <i class="bi bi-chevron-down"></i></a>
+                <ul>
+                <li><a href="./signup.php">Kayıt Ol</a></li>
+                <li><a href="./kayitformu.php">Giriş Yap</a></li>
+                </ul>
+            </li>
+            </ul>
+            <i class="bi bi-list mobile-nav-toggle"></i>
+        </nav><!-- .navbar -->
+
+        </div>
+    </header><!-- End Header --> 
+    
+    <!-- ======= Hero Section ======= -->
+    <section id="hero">
+        <div class="hero-container" data-aos="fade-up" data-aos-delay="140">
+        <h1 >Henry.</h1>
+        <h2>En sevilen teknolojik ürünlere ulaşmanın en güvenli yolu</h2>
+        </div>
+    </section><!-- End Hero -->
+
+
+        <!-- ======= About Section ======= -->
+        <section id="about" class="about">
+      <div class="container" data-aos="fade-up">
+
+        <div class="row justify-content-end">
+          <div class="col-lg-11">
+            <div class="row justify-content-end">
+
+              <div class="col-lg-3 col-md-5 col-6 d-md-flex align-items-md-stretch">
+                <div class="count-box">
+                  <i class="bi bi-emoji-smile"></i>
+                  <span data-purecounter-start="0" data-purecounter-end="5000000" data-purecounter-duration="1" class="purecounter"></span>
+                  <p>Mutlu Müşteriler</p>
+                </div>
+              </div>
+
+              <div class="col-lg-3 col-md-5 col-6 d-md-flex align-items-md-stretch">
+                <div class="count-box">
+                  <i class="bi bi-journal-richtext"></i>
+                  <span data-purecounter-start="0" data-purecounter-end="15" data-purecounter-duration="1" class="purecounter"></span>
+                  <p>Ürün</p>
+                </div>
+              </div>
+
+              <div class="col-lg-3 col-md-5 col-6 d-md-flex align-items-md-stretch">
+                <div class="count-box">
+                  <i class="bi bi-clock"></i>
+                  <span data-purecounter-start="0" data-purecounter-end="25" data-purecounter-duration="1" class="purecounter"></span>
+                  <p>Deneyim Yılı</p>
+                </div>
+              </div>
+
+              <div class="col-lg-3 col-md-5 col-6 d-md-flex align-items-md-stretch">
+                <div class="count-box">
+                  <i class="bi bi-award"></i>
+                  <span data-purecounter-start="0" data-purecounter-end="54" data-purecounter-duration="1" class="purecounter"></span>
+                  <p>Ödüller</p>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-6 video-box align-self-baseline" data-aos="zoom-in" data-aos-delay="100">
+            <img src="assets1/img/about.jpg" class="img-fluid" alt="">
+            
+          </div>
+
+          <div class="col-lg-6 pt-3 pt-lg-0 content">
+            <h3>Henry ile istediğiniz teknolojiye en güvenli ve en hızlı yoldan ulaşabilirsiniz.</h3>
+            <p class="fst-italic">
+              Henry kurulduğu ilk yıldan itibaren yenilikçi bakış açısıyla hızla gelişmektedir.
+            </p>
+            <ul>
+              <li><i class="bx bx-check-double"></i> Yenilikçi teknoloji</li>
+              <li><i class="bx bx-check-double"></i> Güvenilir teknolojik ürünler</li>
+              <li><i class="bx bx-check-double"></i> Hızlı ve güvenilir teslimat</li>
+              <li><i class="bx bx-check-double"></i> Teknolojide söz sahibi firmalar ile iş birliği</li>
+            </ul>
+            <p>
+              Henry ile yeni çıkan teknolojik ürünlere kolayca erişebilirsiniz.
+            </p>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+
+    <!-- ======= About Boxes Section ======= -->
+    <section id="about-boxes" class="about-boxes">
+      <div class="container" data-aos="fade-up">
+
+        <div class="row">
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
+            <div class="card">
+              
+             
+              <div class="card-body" id="rehber1">
+                <h5 class="card-title"><a href="">Yönetici Rehberi</a></h5>
+                <p class="card-text">Yönetici olarak kayıt olduğunuz takdirde,ürün kaydı,silmesi ve güncellemesi yapabilir ayrıca Henry ürünlerini
+                  listeleyebilirsiniz.Ayrıca çalışanlarınızın kaydettiği satışları da inceleyebilirsiniz.Ve en önemlisi Henry için analiz yapabilirsiniz.
+                  en çok satış yapan çalışanınızı ve en çok satılan ürününüzü tespit edebilirsiniz.
+                  Ayrıca sayfamızdaki başarılarımzı inceleyebilirsinzi.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="200">
+            <div class="card">
+              
+              
+              <div class="card-body" id="rehber2" >
+                <h5 class="card-title"><a href="">Çalışan Rehberi</a></h5>
+                <p class="card-text">
+                  Çalışan olarak giriş yaptığınızda her bir Henry ürünü satışını kaydetmelisiniz.İstediğiniz takdirde satış kayıtlarınızı listeleyebilirsiniz.
+                  Ayrıca sayfamızdaki başarılarımzı inceleyebilirsinzi.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="300">
+            <div class="card">
+              
+              
+              <div class="card-body" id="rehber3">
+                <h5 class="card-title"><a href=""> Kullanıcı Rehberi</a></h5>
+                <p class="card-text">Ürün, satış gibi bilgilerimize erişebilmek için giriş yapmanız gerekmektedir.</p>
+              </div>
+
+              <div class="card-body" id="github">
+                <h5 class="card-title"><a href="https://github.com/EmineSener/henry"> Kaynak Kod</a></h5>
+                <p style="text-align:center">Github repo adresi</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Boxes Section -->
+
+
+     <!-- Vendor JS Files -->
+  <script src="assets1/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets1/vendor/aos/aos.js"></script>
+  <script src="assets1/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets1/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets1/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets1/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets1/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets1/js/main.js"></script>
+
+</body>
